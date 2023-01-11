c6ac294793434be00af25ade8b22554988738ca9
Na Cai <caina89@gmail.com>
2023-01-11 22:58:19 +0100

    Update downsample.R

--- PRS_pleio/downsample.R
+++ PRS_pleio/downsample.R
@@ -1,7 +1,11 @@
 library(ggplot2)
+library(plyr)
 load("utilities.R")
+cols=c("#f4a582","#ca0020","#404040")
 
 ## get data 
+phenos=read.table("sigphenos.txt")
+phenos=c(as.character(phenos$x),"LifetimeMDD")
 tests=c(1:10)
 data=NULL
 prss=c("downsample.GPpsy.50k", "downsample.GPpsy.100k", "downsample.SoftImpAll.50k","downsample.SoftImpAll.100k", "GPpsy", "SoftImpAll")
@@ -35,7 +39,6 @@ data$ratio=data$PRS.R2/data$MDD.R2
 write.table(data,paste0(wdir,"/Downsampling.PRSPleio.txt"),sep="\t",quote=F,row.names=T,col.names=T)
 
 ## get PRS Pleiotropy across folds 
-library(plyr)
 dsummary=ddply(data,.(PRSPheno,SampleSize,pheno),summarise,meanp=mean(P),meanratio=mean(ratio),sdratio=sd(ratio),meanr2=mean(PRS.R2),cvr2=sd(PRS.R2/mean(PRS.R2)),meanthresh=mean(Threshold),meanlogp=mean(-log10(Threshold)), sdthresh=sd(Threshold),sdlogp=sd(-log10(Threshold)), sdsnp=sd(Num_SNP),meansnp=mean(Num_SNP))
 dsummary$lower=dsummary$meanratio-dsummary$sdratio*1.96
 dsummary$upper=dsummary$meanratio+dsummary$sdratio*1.96
@@ -61,25 +64,25 @@ g1$SampleSize="DS_50K"
 cor.test(g1$meanratio,g1$dsmeanratio)
 #         Pearson's product-moment correlation
 # data:  g1$meanratio and g1$dsmeanratio
-# t = 33.967, df = 102, p-value < 2.2e-16
+# t = 24.472, df = 61, p-value < 2.2e-16
 # alternative hypothesis: true correlation is not equal to 0
 # 95 percent confidence interval:
-#  0.9393436 0.9717330
+#  0.9226803 0.9711893
 # sample estimates:
 #       cor 
-# 0.9585279 
+# 0.9526597 
 
 ## test for difference between full and downsampled (50K) PRS pleiotropy measures for GPpsy 
 t.test(g1$meanratio,g1$dsmeanratio,paired=T,alternative="two.sided") 
 #         Paired t-test
 # data:  g1$meanratio and g1$dsmeanratio
-# t = -10.154, df = 103, p-value < 2.2e-16
+# t = -8.0562, df = 62, p-value = 3.162e-11
 # alternative hypothesis: true difference in means is not equal to 0
 # 95 percent confidence interval:
-#  -0.10069286 -0.06778622
+#  -0.12784747 -0.07701512
 # sample estimates:
 # mean of the differences 
-#             -0.08423954 
+#              -0.1024313 
 
 ## for 100K downsampling 
 g2=gppsy[,c("pheno","orderpleio","meanratio")]
@@ -92,25 +95,25 @@ g2$SampleSize="DS_100K"
 cor.test(g2$meanratio,g2$dsmeanratio)
 #         Pearson's product-moment correlation
 # data:  g2$meanratio and g2$dsmeanratio
-# t = 71.648, df = 102, p-value < 2.2e-16
+# t = 53.558, df = 61, p-value < 2.2e-16
 # alternative hypothesis: true correlation is not equal to 0
 # 95 percent confidence interval:
-#  0.9855745 0.9933619
+#  0.9826987 0.9936771
 # sample estimates:
 #       cor 
-# 0.9902107 
+# 0.9895337 
 
 ## test for difference between full and downsampled (100K) PRS pleiotropy measures for GPpsy 
 t.test(g2$meanratio,g2$dsmeanratio,paired=T,alternative="two.sided")
 #         Paired t-test
 # data:  g2$meanratio and g2$dsmeanratio
-# t = -0.54098, df = 103, p-value = 0.5897
+# t = 0.35008, df = 62, p-value = 0.7275
 # alternative hypothesis: true difference in means is not equal to 0
 # 95 percent confidence interval:
-#  -0.006651560  0.003800539
+#  -0.006766895  0.009640266
 # sample estimates:
 # mean of the differences 
-#            -0.001425511 
+#             0.001436685
 
 ## prepare plotting data 
 forplot1=rbind(g1,g2)
@@ -138,25 +141,25 @@ s1$SampleSize="DS_50K"
 cor.test(s1$meanratio,s1$dsmeanratio)
 #         Pearson's product-moment correlation
 # data:  s1$meanratio and s1$dsmeanratio
-# t = 45.731, df = 112, p-value < 2.2e-16
+# t = 34.353, df = 61, p-value < 2.2e-16
 # alternative hypothesis: true correlation is not equal to 0
 # 95 percent confidence interval:
-#  0.9628635 0.9821810
+#  0.9590592 0.9849237
 # sample estimates:
 #       cor 
-# 0.9742529
+# 0.9751159
 
 ## test for difference between full and downsampled (50K) PRS pleiotropy measures for SoftImpAll 
 t.test(s1$meanratio,s1$dsmeanratio,paired=T,alternative="two.sided")
 #         Paired t-test
 # data:  s1$meanratio and s1$dsmeanratio
-# t = -3.1774, df = 113, p-value = 0.001917
+# t = -0.1277, df = 62, p-value = 0.8988
 # alternative hypothesis: true difference in means is not equal to 0
 # 95 percent confidence interval:
-#  -0.01727090 -0.00400481
+#  -0.011030573  0.009705839
 # sample estimates:
 # mean of the differences 
-#             -0.01063786
+#           -0.0006623668
 
 ## 100K downsampling 
 s2=soft[,c("pheno","orderpleio","meanratio")]
@@ -169,25 +172,25 @@ s2$SampleSize="DS_100K"
 cor.test(s2$meanratio,s2$dsmeanratio)
 #         Pearson's product-moment correlation
 # data:  s2$meanratio and s2$dsmeanratio
-# t = 92.316, df = 112, p-value < 2.2e-16
+# t = 67.789, df = 61, p-value < 2.2e-16
 # alternative hypothesis: true correlation is not equal to 0
 # 95 percent confidence interval:
-#  0.9905740 0.9955101
+#  0.9891225 0.9960329
 # sample estimates:
-#      cor 
-# 0.993493 
+#       cor 
+# 0.9934281
 
 ## test for difference between full and downsampled (100K) PRS pleiotropy measures for SoftImpAll 
 t.test(s2$meanratio,s2$dsmeanratio,paired=T,alternative="two.sided")
 #         Paired t-test
 # data:  s2$meanratio and s2$dsmeanratio
-# t = -0.65898, df = 113, p-value = 0.5112
+# t = 0.3896, df = 62, p-value = 0.6982
 # alternative hypothesis: true difference in means is not equal to 0
 # 95 percent confidence interval:
-#  -0.004316179  0.002161546
+#  -0.004183885  0.006209598
 # sample estimates:
 # mean of the differences 
-#            -0.001077317
+#             0.001012857 
 
 ## prepare plotting data 
 forplot3=rbind(s1,s2)
@@ -197,25 +200,25 @@ forplot4=forplot4[which(forplot4$pheno%in%spheno),]
 forplot4$order=soft$orderpleio[match(forplot4$pheno,soft$pheno)]
 forplot4$SampleSize=factor(forplot4$SampleSize, levels=c("DS_50K","DS_100K","Full"))
 
-## plot (Supplementary Figure 10)
+## plot (Extended Data Figure 5)
 pdf("Downsample.PRSPleio.pdf",height=6,width=8,useDingbats=FALSE)
 p1=ggplot(forplot1,aes(meanratio,dsmeanratio,colour=SampleSize))+geom_point()+geom_smooth(method="lm")+theme_bw()
-p1=p1+ylim(0,1)+xlim(0,1)+geom_abline(intercept = 0, slope = 1)+ggtitle("GPpsy")+scale_colour_brewer(palette="Set1")
+p1=p1+ylim(0,1)+xlim(0,1)+geom_abline(intercept = 0, slope = 1)+ggtitle("GPpsy")+scale_colour_manual(values=cols[1:2])
 p1=p1+xlab("FullSample/nPRS Pleiotropy") + ylab("Downsampled\nPRS Pleiotropy")
 p1=p1+theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
 panel.background = element_blank(), axis.line = element_line(colour = "black"), legend.position="right")
 p2=ggplot(forplot2,aes(order,meanratio,colour=SampleSize))+geom_point()+geom_line()+theme_bw()
-p2=p2+scale_colour_brewer(palette="Set1")+ylim(0,1)+xlim(0,100)+ggtitle("GPpsy")
+p2=p2+scale_colour_manual(values=cols)+ylim(0,1)+xlim(0,65)+ggtitle("GPpsy")
 p2=p2+xlab("Phenotype") + ylab("PRS Pleiotropy")
 p2=p2+theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
 panel.background = element_blank(), axis.line = element_line(colour = "black"), legend.position="right")
 p3=ggplot(forplot3,aes(meanratio,dsmeanratio,colour=SampleSize))+geom_point()+geom_smooth(method="lm")+theme_bw()
-p3=p3+ylim(0,1)+xlim(0,1)+geom_abline(intercept = 0, slope = 1)+ggtitle("SoftImpAll")+scale_colour_brewer(palette="Set1")
+p3=p3+ylim(0,1)+xlim(0,1)+geom_abline(intercept = 0, slope = 1)+ggtitle("SoftImpAll")+scale_colour_manual(values=cols[1:2])
 p3=p3+xlab("FullSample/nPRS Pleiotropy") + ylab("Downsampled\nPRS Pleiotropy")
 p3=p3+theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
 panel.background = element_blank(), axis.line = element_line(colour = "black"), legend.position="right")
 p4=ggplot(forplot4,aes(order,meanratio,colour=SampleSize))+geom_point()+geom_line()+theme_bw()
-p4=p4+scale_colour_brewer(palette="Set1")+ylim(0,1)+xlim(0,100)+ggtitle("SoftImpAll")
+p4=p4+scale_colour_manual(values=cols)+ylim(0,1)+xlim(0,65)+ggtitle("SoftImpAll")
 p4=p4+xlab("Phenotype") + ylab("PRS Pleiotropy")
 p4=p4+theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
 panel.background = element_blank(), axis.line = element_line(colour = "black"), legend.position="right")
@@ -228,45 +231,45 @@ forplot1=forplot1[which(forplot1$pheno%in%gpheno),]
 forplot1$order=gppsy$orderpleio[match(forplot1$pheno,gppsy$pheno)]
 forplot1$SampleSize=factor(forplot1$SampleSize, levels=c("DS_50K","DS_100K","Full"))
 
-## plot (Supplementary Figure 11)
-pdf("GPpsy.Pthreshold.pdf",height=8,width=12,useDingbats=FALSE)
+## plot (Supplementary Figure 10)
+pdf("GPpsy.Pthreshold.pdf",height=8,width=9,useDingbats=FALSE)
 p1=ggplot(forplot1,aes(pheno,meanthresh,group=SampleSize,colour=SampleSize))+geom_point()+geom_smooth()
 p1=p1+theme_bw()+theme(axis.text.x=element_blank())
-p1=p1+scale_colour_brewer(palette="Set1")+guides(colour=guide_legend(nrow=2,byrow=TRUE))
+p1=p1+scale_colour_manual(values=cols)
 p1=p1+xlab("Phenotype") + ylab("Mean (P Threshold)")
 p1=p1+theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
-panel.background = element_blank(), axis.line = element_line(colour = "black"), legend.position="bottom")
+panel.background = element_blank(), axis.line = element_line(colour = "black"), legend.position="right")
 p3=ggplot(forplot1,aes(pheno,sdthresh,group=SampleSize,colour=SampleSize))+geom_point()+geom_smooth()
 p3=p3+theme_bw()+theme(axis.text.x=element_blank())
-p3=p3+scale_colour_brewer(palette="Set1")+guides(colour=guide_legend(nrow=2,byrow=TRUE))
+p3=p3+scale_colour_manual(values=cols)
 p3=p3+xlab("Phenotype") + ylab("SD (P Threshold)")
 p3=p3+theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
-panel.background = element_blank(), axis.line = element_line(colour = "black"), legend.position="bottom")
+panel.background = element_blank(), axis.line = element_line(colour = "black"), legend.position="right")
 p5=ggplot(forplot1,aes(pheno,meansnp,group=SampleSize,colour=SampleSize))+geom_point()+geom_smooth()
 p5=p5+theme_bw()+theme(axis.text.x=element_blank())
-p5=p5+scale_colour_brewer(palette="Set1")+guides(colour=guide_legend(nrow=2,byrow=TRUE))
+p5=p5+scale_colour_manual(values=cols)
 p5=p5+xlab("Phenotype") + ylab("Mean (NSNPs)")
 p5=p5+theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
-panel.background = element_blank(), axis.line = element_line(colour = "black"), legend.position="bottom")
+panel.background = element_blank(), axis.line = element_line(colour = "black"), legend.position="right")
 p6=ggplot(forplot1,aes(pheno,sdsnp,group=SampleSize,colour=SampleSize))+geom_point()+geom_smooth()
 p6=p6+theme_bw()+theme(axis.text.x=element_blank())
-p6=p6+scale_colour_brewer(palette="Set1")+guides(colour=guide_legend(nrow=2,byrow=TRUE))
+p6=p6+scale_colour_manual(values=cols)
 p6=p6+xlab("Phenotype") + ylab("SD (NSNPs)")
 p6=p6+theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
-panel.background = element_blank(), axis.line = element_line(colour = "black"), legend.position="bottom")
+panel.background = element_blank(), axis.line = element_line(colour = "black"), legend.position="right")
 p7=ggplot(forplot1,aes(pheno,cvr2,group=SampleSize,colour=SampleSize))+geom_point()+geom_smooth()
 p7=p7+theme_bw()+theme(axis.text.x=element_blank())
-p7=p7+scale_colour_brewer(palette="Set1")+guides(colour=guide_legend(nrow=2,byrow=TRUE))
+p7=p7+scale_colour_manual(values=cols)
 p7=p7+xlab("Phenotype") + ylab("SD (R2/mean(R2))")
 p7=p7+theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
-panel.background = element_blank(), axis.line = element_line(colour = "black"), legend.position="bottom")
+panel.background = element_blank(), axis.line = element_line(colour = "black"), legend.position="right")
 p8=ggplot(forplot1,aes(pheno,sdratio,group=SampleSize,colour=SampleSize))+geom_point()+geom_smooth()
 p8=p8+theme_bw()+theme(axis.text.x=element_blank())
-p8=p8+scale_colour_brewer(palette="Set1")+guides(colour=guide_legend(nrow=2,byrow=TRUE))
+p8=p8+scale_colour_manual(values=cols)
 p8=p8+xlab("Phenotype") + ylab("SD (PRS Pleiotropy)")
 p8=p8+theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
-panel.background = element_blank(), axis.line = element_line(colour = "black"), legend.position="bottom")
-multiplot(p1,p6,p3,p7,p5,p8,cols=3)
+panel.background = element_blank(), axis.line = element_line(colour = "black"), legend.position="right")
+multiplot(p1,p5,p7,p3,p6,p8,cols=2)
 dev.off()
 
 ## SoftImpAll: do sample size affect p value thresholds 
@@ -275,45 +278,48 @@ forplot1=forplot1[which(forplot1$pheno%in%spheno),]
 forplot1$order=soft$orderpleio[match(forplot1$pheno,soft$pheno)]
 forplot1$SampleSize=factor(forplot1$SampleSize, levels=c("DS_50K","DS_100K","Full"))
 
-pdf("SoftImpAll.Pthreshold.pdf",height=8,width=12,useDingbats=FALSE)
+## plot (Supplementary Figure 11)
+pdf("SoftImpAll.Pthreshold.pdf",height=8,width=9,useDingbats=FALSE)
 p1=ggplot(forplot1,aes(pheno,meanthresh,group=SampleSize,colour=SampleSize))+geom_point()+geom_smooth()
 p1=p1+theme_bw()+theme(axis.text.x=element_blank())
-p1=p1+scale_colour_brewer(palette="Set1")+guides(colour=guide_legend(nrow=2,byrow=TRUE))
+p1=p1+scale_colour_manual(values=cols)
 p1=p1+xlab("Phenotype") + ylab("Mean (P Threshold)")
 p1=p1+theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
-panel.background = element_blank(), axis.line = element_line(colour = "black"), legend.position="bottom")
+panel.background = element_blank(), axis.line = element_line(colour = "black"), legend.position="right")
 p3=ggplot(forplot1,aes(pheno,sdthresh,group=SampleSize,colour=SampleSize))+geom_point()+geom_smooth()
 p3=p3+theme_bw()+theme(axis.text.x=element_blank())
-p3=p3+scale_colour_brewer(palette="Set1")+guides(colour=guide_legend(nrow=2,byrow=TRUE))
+p3=p3+scale_colour_manual(values=cols)
 p3=p3+xlab("Phenotype") + ylab("SD (P Threshold)")
 p3=p3+theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
-panel.background = element_blank(), axis.line = element_line(colour = "black"), legend.position="bottom")
+panel.background = element_blank(), axis.line = element_line(colour = "black"), legend.position="right")
 p5=ggplot(forplot1,aes(pheno,meansnp,group=SampleSize,colour=SampleSize))+geom_point()+geom_smooth()
 p5=p5+theme_bw()+theme(axis.text.x=element_blank())
-p5=p5+scale_colour_brewer(palette="Set1")+guides(colour=guide_legend(nrow=2,byrow=TRUE))
+p5=p5+scale_colour_manual(values=cols)
 p5=p5+xlab("Phenotype") + ylab("Mean (NSNPs)")
 p5=p5+theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
-panel.background = element_blank(), axis.line = element_line(colour = "black"), legend.position="bottom")
+panel.background = element_blank(), axis.line = element_line(colour = "black"), legend.position="right")
 p6=ggplot(forplot1,aes(pheno,sdsnp,group=SampleSize,colour=SampleSize))+geom_point()+geom_smooth()
 p6=p6+theme_bw()+theme(axis.text.x=element_blank())
-p6=p6+scale_colour_brewer(palette="Set1")+guides(colour=guide_legend(nrow=2,byrow=TRUE))
+p6=p6+scale_colour_manual(values=cols)
 p6=p6+xlab("Phenotype") + ylab("SD (NSNPs)")
 p6=p6+theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
-panel.background = element_blank(), axis.line = element_line(colour = "black"), legend.position="bottom")
+panel.background = element_blank(), axis.line = element_line(colour = "black"), legend.position="right")
 p7=ggplot(forplot1,aes(pheno,cvr2,group=SampleSize,colour=SampleSize))+geom_point()+geom_smooth()
 p7=p7+theme_bw()+theme(axis.text.x=element_blank())
-p7=p7+scale_colour_brewer(palette="Set1")+guides(colour=guide_legend(nrow=2,byrow=TRUE))
+p7=p7+scale_colour_manual(values=cols)
 p7=p7+xlab("Phenotype") + ylab("SD (R2/mean(R2))")
 p7=p7+theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
-panel.background = element_blank(), axis.line = element_line(colour = "black"), legend.position="bottom")
+panel.background = element_blank(), axis.line = element_line(colour = "black"), legend.position="right")
 p8=ggplot(forplot1,aes(pheno,sdratio,group=SampleSize,colour=SampleSize))+geom_point()+geom_smooth()
 p8=p8+theme_bw()+theme(axis.text.x=element_blank())
-p8=p8+scale_colour_brewer(palette="Set1")+guides(colour=guide_legend(nrow=2,byrow=TRUE))
+p8=p8+scale_colour_manual(values=cols)
 p8=p8+xlab("Phenotype") + ylab("SD (PRS Pleiotropy)")
 p8=p8+theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
-panel.background = element_blank(), axis.line = element_line(colour = "black"), legend.position="bottom")
-multiplot(p1,p6,p3,p7,p5,p8,cols=3)
+panel.background = element_blank(), axis.line = element_line(colour = "black"), legend.position="right")
+multiplot(p1,p5,p7,p3,p6,p8,cols=2)
 dev.off()
+
+
 
 
 
